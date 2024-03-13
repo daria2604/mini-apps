c4cd5d7ac64b47eef5f8fa331852901021ffdac6
Дарья Рутц <darya_r_qschool@qsoft.ru>
2024-03-13 16:58:40 +0300

    feat: выводить факт по кнопке

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,42 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [fact, setFact] = useState('');
+
+  function fetchCatFacts() {
+    fetch('https://catfact.ninja/fact')
+      .then((res) => res.json())
+      .then((data) => {
+        setFact(data.fact);
+        setCursor(data.fact);
+      });
+  }
+
+  function setCursor(sentence) {
+    const words = sentence.split(' ');
+    const firstWord = words[0];
+    const restOfSentence = sentence.substring(firstWord.length + 1);
+
+    let inputElement = document.getElementById('catFact');
+    inputElement.focus();
+    inputElement.value = firstWord + ' ' + restOfSentence;
+    inputElement.setSelectionRange(firstWord.length, firstWord.length);
+  }
+
+  return (
+    <>
+      <div>
+        <button onClick={fetchCatFacts}>get a fact</button>
+        <textarea
+          name='catfact'
+          id='catFact'
+          cols='30'
+          rows='10'
+          defaultValue={fact}
+        ></textarea>
+      </div>
+    </>
+  );
 }
 
 export default App;
